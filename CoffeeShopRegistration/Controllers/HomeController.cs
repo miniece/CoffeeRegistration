9a01539611180e1616495933698725819b04019f
miniece <mvelinda@icloud.com>
2022-10-27 17:29:00 -0400

    commit for lab

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Result(RegistrationClass r)
+        {
+            return View(r);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistration/Models/RegistrationClass.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShopRegistration.Models
+{
+    public class RegistrationClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
